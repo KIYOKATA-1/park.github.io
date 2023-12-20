41149befd4ebb646692573c555794567690bdf96
Pavel <112262251+KIYOKATA-1@users.noreply.github.com>
2023-12-20 23:58:05 +0600

    pavel

---
+++ main.js
@@ -0,0 +1,29 @@
+
+window.addEventListener('DOMContentLoaded', function(){
+    onload = () => {
+        const c = setTimeout(() => {
+          document.body.classList.remove("not-loaded");
+          clearTimeout(c);
+        }, 1000);
+      };
+
+
+      const btn = document.getElementById('fl_btn');
+
+      let flower = document.querySelector('.flowers');
+      let night = document.querySelector('.night');
+      const myAudio = document.getElementById("myAudio");
+      let pavel = document.querySelector('.pavel');
+
+      btn.addEventListener('click', () =>{
+        flower.style.opacity = 1;
+        night.style.opacity = 1;
+
+        btn.style.display = 'none';
+
+        pavel.style.opacity = 1;
+
+        myAudio.src = 'flower.mp3';
+        myAudio.play();
+      });
+})
