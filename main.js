2e9aa717db7b37f9222e5cb57daba42ff9f434d7
Pavel <112262251+KIYOKATA-1@users.noreply.github.com>
2023-12-21 01:36:39 +0600

    fffffffffff

--- main.js
+++ main.js
@@ -21,11 +21,12 @@ window.addEventListener('DOMContentLoaded', function(){
         btn.style.display = 'none';
         myAudio.src = 'flower.mp3';
         myAudio.play();
+        audio.addEventListener('ended', function (e) {
+          audio.currentTime = 0;
+          audio.play();
       });
-      audio.addEventListener('ended', function () {
-        audio.currentTime = 0;
-        audio.play();
-    });
+      });
+
       
 });
 
